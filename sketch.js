3cd5b05e969a0edfdc18c9a7a4ffba0b43be5e1b
Gabriely Souza <gabriely.gomes06@escola.pr.gov.br>
2025-05-29 08:47:56 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(200,0,150);
+  fill(100, 0, 300)
+  circle(200,200,250)
+  stroke(200,0,0)
+  fill(255,255,255)
+  circle(160,170,40)
+  circle(250,170,40)
+triangle(220,235,200, 200, 180, 235)
+  line()
+  circle(mouseX, mouseY, 40)
+}
